3415e2ea35bb538288a3f38face8764000a024c8
Valentin Cadart <cadart.valentin@sfr.fr>
2022-03-31 11:35:07 +0200

    ajout entity categorie et commentaires

---
+++ migrations/Version20220331075633.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220331075633 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, article_id INT NOT NULL, author VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_5F9E962A7294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A7294869C FOREIGN KEY (article_id) REFERENCES articles (id)');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168A21214B7 FOREIGN KEY (categories_id) REFERENCES categories (id)');
+        $this->addSql('CREATE INDEX IDX_BFDD3168A21214B7 ON articles (categories_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD3168A21214B7');
+        $this->addSql('DROP INDEX IDX_BFDD3168A21214B7 ON articles');
+    }
+}
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Articles;
+use App\Entity\Categories;
+use App\Entity\Comments;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,15 +12,46 @@ class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i=1; $i <=10 ; $i++) { 
-            $article = new Articles();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("Contenu de l'article n°$i")
-                    ->setImage("https://via.placeholder.com/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($article);
-            
+        //créer 3 catégories fakées
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Categories();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+            $manager->persist($category);
+
+            //créer entre 4 et 6 articles
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Articles();
+
+
+                $content =  join($faker->paragraphs(5), ) ;
+
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategories($category);
+
+                $manager->persist($article);
+
+                //on donne des commentaires a l'article
+                for ($k = 0; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comments();
+
+                    $content = join($faker->paragraphs(2));
+
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content)
+                        ->setCreatedAt($faker->dateTimeBetween('-' . $days . 'days'))
+                        ->setArticle($article);
+                        $manager->persist($comment);
+                }
+            }
         }
 
         $manager->flush();
